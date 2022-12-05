47ee63518a76cc1dc7212691e96cda9f0eb3f5a3
Cris Maillo <cris@Criss-MacBook-Air.local>
2022-12-05 14:03:34 +0000

    finished day 3 pt 2

--- day3/script_pt2.js
+++ day3/script_pt2.js
@@ -1,30 +1,32 @@
 var fs = require('fs');
 const { stringify } = require('querystring');
-var item1, item2;
+var item1, item2,item3;
 var common_char = [];
+let groups = []
 let totalPriority = 0;
 
 fs.readFile('input.txt', 'utf8', function(err, data){
     
     rucksacks = data.split('\n');
-    for (var i = 0; i < rucksacks.length; i++){
-        var rucksack = rucksacks[i];
+    numGrups = rucksacks.length / 3;
+    for (var i = 0; i < rucksacks.length; i+= 3){
 
-        item1 = rucksack.slice(0, (rucksack.length/2))
-        item2 = rucksack.slice((rucksack.length/2))
-
-        findItemMatch(item1, item2)
+        item1 = rucksacks[i]
+        item2 = rucksacks[i + 1]
+        item3 = rucksacks[i + 2]
+        findItemMatch(item1, item2, item3)
     }
     prioritizeItems(common_char)
 })
 
-function findItemMatch(item1, item2){
+function findItemMatch(item1, item2, item3){
 
     for (let i=0; i < item1.length; i++){
-        if(item2.includes(item1[i])){
+        if(item2.includes(item1[i]) & item3.includes(item1[i])){
             var character = item1[i]
         }
     }
+    console.log(character)
     common_char.push(character)
 }
 
